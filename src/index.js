1488210cdc9042d006624442e8cc9d47188eee49
Mark Sandora <marksandora@trueearthhealth.com>
2020-08-21 05:08:58 -0400

    end of section 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Hot Module Replacement for better development experience
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
