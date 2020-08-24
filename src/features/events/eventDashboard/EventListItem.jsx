2ef0abafc4e62124f840886f4d1949045690d305
Mark Sandora <marksandora@trueearthhealth.com>
2020-08-24 12:09:07 -0400

    End of Section 4

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
-export default function EventListItem({ event }) {
+export default function EventListItem({ event, selectEvent, deleteEvent }) {
   return (
     <Segment.Group>
       <Segment>
@@ -29,9 +29,22 @@ export default function EventListItem({ event }) {
           ))}
         </List>
       </Segment>
+      <Segment>
+        <div style={{ whiteSpace: 'pre-wrap' }}>{event.description}</div>
+      </Segment>
       <Segment clearing>
-        <div>{event.description}</div>
-        <Button content='View' color='teal' floated='right' />
+        <Button
+          onClick={() => deleteEvent(event.id)}
+          content='Delete'
+          color='red'
+          floated='right'
+        />
+        <Button
+          onClick={() => selectEvent(event)}
+          content='View'
+          color='teal'
+          floated='right'
+        />
       </Segment>
     </Segment.Group>
   );
