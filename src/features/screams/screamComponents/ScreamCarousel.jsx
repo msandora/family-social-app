680ada570c93c2ac51e13cde9e17f7fba87cc40e
Muhammed khaled <muhammmed.k95@gmail.com>
2020-10-26 03:53:50 +0200

    fixing screamCarousel and making develop branch

--- src/features/screams/screamComponents/ScreamCarousel.jsx
+++ src/features/screams/screamComponents/ScreamCarousel.jsx
@@ -3,19 +3,31 @@ import { Image } from 'semantic-ui-react';
 import Carousel from 'semantic-ui-carousel-react';
 
 const ScreamCarousel = ({ scream }) => {
-  console.log('carousel', scream);
-
-  let elements = [
-    {
+  console.log({scream})
+  // <Image src={`/assets/categoryImages/travel.jpg`} fluid />
+  const defaultImg ="/assets/categoryImages/travel.jpg";
+  let elements = [];
+  scream.screamImages &&  scream.screamImages.map((img) => {
+    elements.push({
       render: () => {
-        return <Image src={`/assets/categoryImages/travel.jpg`} fluid />;
-      },
+   return  <Image src={img} alt="img" fluid /> 
+ },
+ }) 
+      });
+  console.log({elements})
+  let DefaultElements = [
+   {
+    render: () => {
+      return <Image src={`/assets/categoryImages/travel.jpg`} fluid />
     },
+  } 
   ];
+  
+  
 
   return (
     <Carousel
-      elements={elements}
+      elements={scream && scream.screamImages ? elements : DefaultElements}
       duration={3000}
       animation='fade'
       showNextPrev={false}
